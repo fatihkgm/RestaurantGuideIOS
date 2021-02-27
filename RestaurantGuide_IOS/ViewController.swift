24333f5163830157ef88f60f27e719c2ad6031ff
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-02-26 23:31:40 -0500

    Initial Commit

---
+++ RestaurantGuide_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantGuide_IOS
+//
+//  Created by Fatih on 2021-02-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
