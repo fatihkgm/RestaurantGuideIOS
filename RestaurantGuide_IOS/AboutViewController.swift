031c7b511fd05d277a2e04a00db13b45db1f6e13
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-28 14:41:43 -0400

    Navigation bar for the first restaurant and about us created

---
+++ RestaurantGuide_IOS/AboutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutViewController.swift
+//  RestaurantGuide_IOS
+//
+//  Created by Fatih on 2021-03-28.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "About Us"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RestaurantGuide_IOS/Res1ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Res1ViewController.swift
+//  RestaurantGuide_IOS
+//
+//  Created by Fatih on 2021-03-28.
+//
+
+import UIKit
+
+class Res1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "McDonals"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
