25f43c14e56851d98f10381321998fab6f5479b1
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-28 23:01:57 -0400

    Navigation View Controller Added for other five restaurant

---
+++ RestaurantGuide_IOS/Res3ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Res3ViewController.swift
+//  RestaurantGuide_IOS
+//
+//  Created by Fatih on 2021-03-28.
+//
+
+import UIKit
+
+class Res3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
