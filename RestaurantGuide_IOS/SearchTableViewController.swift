224d50ec59aa69376a22c1f963f670e83dab8851
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-04-14 15:40:17 -0400

    Search table created

---
+++ RestaurantGuide_IOS/SearchTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SearchTableViewController.swift
+//  RestaurantGuide_IOS
+//
+//  Created by Fatih on 2021-04-14.
+//
+
+import UIKit
+
+class SearchTableViewController: UITableViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title="Seach Your Restaurant"
+        
+
+        
+    }
+
+ 
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        return 0
+    }
+
+    
+
+}
---
+++ SearchTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SearchTableViewController.swift
+//  RestaurantGuide_IOS
+//
+//  Created by Fatih on 2021-04-12.
+//
+
+import UIKit
+
+class SearchTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title="Search the Restaurant"
+
+
+    }
+
+ 
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+ 
+
+}
