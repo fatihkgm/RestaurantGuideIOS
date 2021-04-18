1e2e4bd1eebd11b6293f8006955c14e05186136e
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-04-18 03:17:44 -0400

    add rest function created

---
+++ RestaurantGuide_IOS/AddRestTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AddRestTableViewCell.swift
+//  RestaurantGuide_IOS
+//
+//  Created by Fatih on 2021-04-18.
+//
+
+import UIKit
+
+class AddRestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
